8a1c37557a4f0d1b5846306c2370587d433f5a88
m.zepeda040803-jpg <m.zepeda040803@gmai.com>
2025-09-25 14:41:37 -0400

    Commit again

---
+++ src/main/java/com/pluralsight/Receipt.java
@@ -0,0 +1,29 @@
+package com.pluralsight;
+
+public class Receipt {
+    public static void main(String[] args) {
+        String fruit = "Blueberry";
+        double itemPrice = 2.28;
+        int quantityBought = 2;
+
+        double totalPrice = itemPrice * quantityBought;
+        System.out.println("I bought" + (fruit) "for" + (totalPrice));
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
