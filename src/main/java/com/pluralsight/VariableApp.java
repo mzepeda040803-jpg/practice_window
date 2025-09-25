154db9e265d9fa70f45dbcc3e830b7f1086f304b
m.zepeda040803-jpg <m.zepeda040803@gmai.com>
2025-09-25 13:28:30 -0400

    commit

---
+++ src/main/java/com/pluralsight/VariableApp.java
@@ -0,0 +1,24 @@
+package com.pluralsight;
+
+public class VariableApp {
+    public static void main(String[] args)  {
+
+        String favoriteColor= "Blue";
+        System.out.println("The answer is" + (favoriteColor));
+        int yearStarted= 2025;
+        System.out.println("The answer is" + (yearStarted));
+        char middleInitial= 'V';
+        System.out.println("The answer is" + (middleInitial));
+        String pets= "true";
+        System.out.println("Do you have a pet?" + (pets));
+        String niceMessage= "You're nice.";
+        System.out.println("You're message is that" + (niceMessage));
+
+
+    }
+
+
+
+}
+
+
