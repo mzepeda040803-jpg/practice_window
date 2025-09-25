db8fe9f8e0702e9eab5ecf0244b9256a8e95f752
m.zepeda040803-jpg <m.zepeda040803@gmai.com>
2025-09-25 14:07:33 -0400

    commit again

---
+++ src/main/java/com/pluralsight/VANextstep.java
@@ -0,0 +1,20 @@
+package com.pluralsight;
+
+import java.util.GregorianCalendar;
+
+public class VANextstep {
+    public static void main(String[] args) {
+        int daysInWeek = 7;
+        System.out.println("there are" + (daysInWeek) + "days in a week");
+        int priceOfCoffee = (int) 4.99;
+        System.out.println("Coffee costs $" + (priceOfCoffee));
+        char favoriteLetter = 'Z';
+        System.out.println("My favorite letter is" + (favoriteLetter));
+        boolean ifRain = true;
+        boolean ifNoRain = false;
+        System.out.println("Is it raining?" + (ifRain));
+
+
+    }
+
+}
--- src/main/java/com/pluralsight/VariableApp.java
+++ src/main/java/com/pluralsight/VariableApp.java
@@ -1,24 +1,20 @@
 package com.pluralsight;
 
 public class VariableApp {
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
 
-        String favoriteColor= "Blue";
+        String favoriteColor = "Blue";
         System.out.println("The answer is" + (favoriteColor));
-        int yearStarted= 2025;
+        int yearStarted = 2025;
         System.out.println("The answer is" + (yearStarted));
-        char middleInitial= 'V';
+        char middleInitial = 'V';
         System.out.println("The answer is" + (middleInitial));
-        String pets= "true";
+        String pets = "Yes";
         System.out.println("Do you have a pet?" + (pets));
-        String niceMessage= "You're nice.";
+        String niceMessage = "You're nice.";
         System.out.println("You're message is that" + (niceMessage));
 
 
     }
 
-
-
 }
-
-
